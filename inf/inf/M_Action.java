b07906d397263cc0277b57e1876ebd4a5924e3c6
ParkIChan <ParkIChan@PARKICHAN>
2019-09-23 19:51:49 +0900

    오후 7:51 2019-09-23 ㄱㄱ

--- inf/inf/M_Action.java
+++
@@ -1,9 +0,0 @@
-package inf;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface M_Action {
-
-	void execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
--- inf/inf/Transfer/Transfer.java
+++
@@ -1,5 +0,0 @@
-package inf.Transfer;
-
-public interface Transfer {
-	void transfer();
-}
