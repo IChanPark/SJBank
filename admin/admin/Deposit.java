63eb949f3c35b6e685a707d317e2fc657ef580a8
Jehni <Jehni@LAPTOP-RK5EMBU6>
2019-10-01 17:11:06 +0900

    오후 5:10 2019-10-01 디포짓수정

--- admin/admin/Deposit.java
+++ admin/admin/Deposit.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import inf.M_Action;
+import jdbc.Deposit.Deposits_infoDAO;
 import jdbc.Deposit.Deposits_infoDTO;
 
 public class Deposit  implements M_Action{
@@ -11,6 +12,6 @@ public class Deposit  implements M_Action{
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 	
 		Deposits_infoDTO dto = new Deposits_infoDTO();
-		request.setAttribute("data", dto); 
+		request.setAttribute("data", Deposits_infoDAO.getInstance().list()); 
 	}
 }
